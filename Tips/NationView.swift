b305d98d57b8f8f0310a4be61341d4b47d7f07c8
EricSFChu <echu6@mail.ccsf.edu>
2015-12-10 23:46:27 -0800

    Implementing a nation view for locality

---
+++ Tips/NationView.swift
@@ -0,0 +1,22 @@
+//
+//  NationView.swift
+//  Tips
+//
+//  Created by Eric Chu on 12/10/15.
+//  Copyright © 2015 Eric Chu. All rights reserved.
+//
+
+import UIKit
+
+class NationView: UIViewController
+{
+    @IBOutlet weak var americanFlag: UIImageView!
+    
+    
+    @IBAction func changeToUSD(sender: AnyObject) {
+    self.view.backgroundColor = UIColor.redColor()
+    }
+    
+    
+    
+}
